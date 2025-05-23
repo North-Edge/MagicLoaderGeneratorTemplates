3cfd2fdf224db4467084a778465f9c1b880bc04a
Xebeth <github@north-edge.com>
2025-05-23 20:37:38 +0100

    Initial commit

---
+++ SolutionTemplate/MagicLoaderConsoleMod/Program.cs
@@ -0,0 +1,18 @@
+﻿using MagicLoaderGenerator.Localization.Providers;
+using MagicLoaderGenerator.Filesystem.Generators;
+using Microsoft.Extensions.Configuration;
+using MagicLoaderGenerator.Filesystem;
+using MagicLoaderGenerator;
+
+var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("config.json", optional: false);
+var appConfig = new AppConfig(builder.Build());
+var mod = new MagicLoaderMod(new JsonLocalizationProvider(appConfig), appConfig);
+
+var outputDir = mod.Generate(new ZipOutputGenerator(appConfig));
+
+System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo {
+    UseShellExecute = true,
+    FileName = outputDir,
+    Verb = "open"
+});
---
+++ readme.md
@@ -0,0 +1,16 @@
+# MagicLoader generator .NET templates
+
+## Description
+These templates are meant to facilitate the creation of mods based o the [MagicLoaderGenerator](https://github.com/North-Edge/MagicLoaderGenerator) library.
+
+## Installing or Updating the templates
+`dotnet new install NorthEdge.MagicLoaderGenerator.Templates`
+
+## Creating a solution
+`dotnet new mlg-sln -n MyMod`
+
+## Creating a console project only
+`dotnet new mlg-mod -n MyMod`
+
+## Uninstalling
+`dotnet new uninstall NorthEdge.MagicLoaderGenerator.Templates`
